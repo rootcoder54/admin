530c749d3197136cd30ee9fee986c96745c75468
ozymandias54 <bassirou718@gmail.com>
2024-12-18 10:49:41 +0000

    dark mode login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
 import { LoginForm } from "@/components/auth/loginForm";
+import { Mode } from "@/components/provider/mode";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div>
+      <div className="absolute right-0 top-0 p-5">
+        <Mode />
+      </div>
+      <LoginForm />
+    </div>
+  );
 }
--- components/provider/mode.tsx
+++ components/provider/mode.tsx
@@ -16,10 +16,10 @@ export function Mode() {
 
 
   return (
-    <SidebarMenuButton onClick={change}>
+    <button onClick={change} className="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span>Theme</span>
-    </SidebarMenuButton>
+    </button>
   );
 }
