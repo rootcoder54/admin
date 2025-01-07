680a887204925ca1adebd50cab4e733404c9a5a0
ozymandias54 <bassirou718@gmail.com>
2025-01-07 16:12:45 +0000

    navbar responsive

--- app/(support)/_components/Navbar.tsx
+++ app/(support)/_components/Navbar.tsx
@@ -2,6 +2,15 @@ import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "@/components/ui/popover";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -27,30 +36,69 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center gap-x-4">
-        {!pathname.startsWith("/formation") && (
-          <Link
-            href="/formation"
-            className={buttonVariants({ variant: "ghost" })}
-          >
-            Formation
-          </Link>
-        )}
-        {!pathname.startsWith("/telechargement") && (
-          <Link
-            href="/telechargement"
-            className={buttonVariants({ variant: "ghost" })}
-          >
-            Telechargement
-          </Link>
-        )}
-        {pathname !== "/support" && (
-          <Link
-            href="/support"
-            className={buttonVariants({ variant: "ghost" })}
-          >
-            Support
-          </Link>
-        )}
+        <div className="hidden md:flex items-center gap-x-4">
+          {!pathname.startsWith("/formation") && (
+            <Link
+              href="/formation"
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              Formation
+            </Link>
+          )}
+          {!pathname.startsWith("/telechargement") && (
+            <Link
+              href="/telechargement"
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              Telechargement
+            </Link>
+          )}
+          {pathname !== "/support" && (
+            <Link
+              href="/support"
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              Support
+            </Link>
+          )}
+        </div>
+        <div className="md:hidden flex">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline">
+                <Menu />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[150px]">
+              <div className="flex flex-col gap-y-2">
+                {!pathname.startsWith("/formation") && (
+                  <Link
+                    href="/formation"
+                    className={buttonVariants({ variant: "ghost" })}
+                  >
+                    Formation
+                  </Link>
+                )}
+                {!pathname.startsWith("/telechargement") && (
+                  <Link
+                    href="/telechargement"
+                    className={buttonVariants({ variant: "ghost" })}
+                  >
+                    Telechargement
+                  </Link>
+                )}
+                {pathname !== "/support" && (
+                  <Link
+                    href="/support"
+                    className={buttonVariants({ variant: "ghost" })}
+                  >
+                    Support
+                  </Link>
+                )}
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
         <div className="border border-zinc-400 h-7" />
         <Link href="/" className={buttonVariants({ variant: "outline" })}>
           Admin
