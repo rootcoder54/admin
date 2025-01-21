804a2ecc6119b42247e2009309ad96c9b9aee9ea
ozymandias54 <bassirou718@gmail.com>
2025-01-21 10:47:22 +0000

    init fullcalendar

--- app/(crm)/(routes)/calendrier/page.tsx
+++ app/(crm)/(routes)/calendrier/page.tsx
@@ -1,16 +1,31 @@
 "use client";
-import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 const Page = () => {
-  const [date, setDate] = useState<Date | undefined>(new Date());
   return (
     <div className="w-full min-h-screen p-4">
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border w-full h-full"
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          interactionPlugin,
+          timeGridPlugin
+        ]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "resourceTimelineWeek,dayGridMonth,timeGridWeek"
+        }}
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        initialEvents={[
+          { title: "nice event", start: new Date(), resourceId: "a" }
+        ]}
       />
     </div>
   );
