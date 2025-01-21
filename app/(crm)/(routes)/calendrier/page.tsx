9a3496522d4111ebe74fc8bf0758b15a60ed449d
ozymandias54 <bassirou718@gmail.com>
2025-01-21 11:02:50 +0000

    date click

--- app/(crm)/(routes)/calendrier/page.tsx
+++ app/(crm)/(routes)/calendrier/page.tsx
@@ -4,16 +4,20 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import frLocale from "@fullcalendar/core/locales/fr";
+import { useState } from "react";
 
 const Page = () => {
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
+  const handleDateClick = (info: any) => {
+    setSelectedDate(info.dateStr);
+    //alert(`Vous avez cliqué sur la date : ${info.dateStr}`);
+  };
   return (
     <div className="w-full min-h-screen p-4">
       <FullCalendar
-        plugins={[
-          dayGridPlugin,
-          interactionPlugin,
-          timeGridPlugin
-        ]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
@@ -24,9 +28,23 @@ const Page = () => {
         selectable={true}
         selectMirror={true}
         initialEvents={[
-          { title: "nice event", start: new Date(), resourceId: "a" }
+          { title: "nice event", start: new Date(), resourceId: "a" },
+          {
+            title: "Maintenance du Mois",
+            start: new Date("2025-01-27"),
+            resourceId: "a"
+          }
         ]}
+        locale={frLocale}
+        dateClick={handleDateClick}
       />
+      {selectedDate && (
+        <div className="mt-4 text-center">
+          <p>
+            Date sélectionnée : <strong>{selectedDate}</strong>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
