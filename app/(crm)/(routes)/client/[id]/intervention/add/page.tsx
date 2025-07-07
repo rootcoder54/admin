546211c99b574a3be18934568d61223bf5f830c1
ozymandias54 <bassirou718@gmail.com>
2025-07-07 14:15:11 +0000

    20250707_1415

--- app/(crm)/(routes)/client/[id]/intervention/add/page.tsx
+++ app/(crm)/(routes)/client/[id]/intervention/add/page.tsx
@@ -9,7 +9,7 @@ const AddIntervention = async ({ params }: ClientIdPageProps) => {
 
   return (
     <div>
-      <AddIntevention id={id} requeteId={undefined} numero="j" />
+      <AddIntevention id={id} requeteId={undefined} client="MaliSystem" />
     </div>
   );
 };
